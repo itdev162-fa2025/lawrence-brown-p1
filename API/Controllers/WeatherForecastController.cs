7b6bb25eea69733ff2d8555437718687ae6d33fd
DJLARRYLOVE <browl191@gmatc.matc.edu>
2024-12-11 19:40:06 -0600

    Completed Project 1 - API Delve

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -41,18 +42,39 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpPost]
-    public ActionResult<WeatherForecast> Create()
+    public ActionResult<WeatherForecast> Create([FromBody] WeatherForecast model)
     {
+        //var tempc=Convert.ToInt16(temp);s
         //View in your VS Code console - fyi
         Console.WriteLine($"Database path: {_context.DbPath}");
         Console.WriteLine ("Insert a new WeatherForecast");
+    
+        double tempc = model.TemperatureC;
+        string summ = "";
+        double tempf = 32 + tempc / 0.5556;
+        if (tempf <= 32){ summ = "Freezing"; }
+        if (tempf > 32 && tempf <= 40) { summ = "Bracing"; ;}
+        if (tempf > 40 && tempf <= 50) { summ = "Chilly"; ; }
+        if (tempf > 50 && tempf <= 60) { summ = "Cool"; ; }
+        if (tempf > 60 && tempf <= 70) { summ = "Mild"; ; }
+        if (tempf > 70 && tempf <= 80) { summ = "Warm"; ; }
+        if (tempf > 80 && tempf <= 90) { summ = "Balmy"; ; }
+        if (tempf > 90 && tempf <= 100) { summ = "Hot"; ; }
+        if (tempf > 100 && tempf <= 110) { summ = "Sweltering"; ; }
+        if (tempf > 110 && tempf <= 140) { summ = "Scorching"; ; }
+
+        //"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 
         var forecast = new WeatherForecast()
         {
-            Date = new DateOnly(),
-            TemperatureC = 75,
-            Summary = "Warm"
+            Date = DateOnly.FromDateTime(DateTime.Now),
+            TemperatureC = model.TemperatureC,
+            Summary = summ
         };
+
+        model.Summary = summ;
+        Console.WriteLine(model.Summary);
+
 
         _context.WeatherForecasts.Add(forecast);
         var success = _context.SaveChanges() > 0;
